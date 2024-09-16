b79b4e6b1fa24e3e64bf3c04fc30c55278ca878e
Aruj Garg <arujgarg04@gmail.com>
2024-09-16 19:14:04 +0530

    app.jsx change reverted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/signup" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
