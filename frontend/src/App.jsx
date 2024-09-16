22d657b787c2235ce63c1e014c482503a868dfe8
Aruj Garg <arujgarg04@gmail.com>
2024-09-16 19:11:10 +0530

    app.jsx file changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signup" />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
